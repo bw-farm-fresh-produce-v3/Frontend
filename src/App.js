28b35599841f445dc1098d2df2a56257bbbc30c0
DeniseFafette <denisefafette@gmail.com>
2020-01-07 13:38:19 -0800

    resolved conflict

--- src/App.js
+++ src/App.js
@@ -11,11 +11,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       <NavBar />
-=======
-        <h1>App is Running</h1>
->>>>>>> b1c5b6799fe59e325bf5053f8dfdec1a6257acd9
       <Switch>
         <Route exact path='/register-form'>
           <RegisterForm />
