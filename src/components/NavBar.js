3bf351122a3b4a1175ac65dc1d12a75f877bf5b5
DeniseFafette <denisefafette@gmail.com>
2020-01-07 14:05:02 -0800

    removed account link and updated href signin link

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,10 +6,9 @@ const NavBar = props => {
             <a href='/'><i class="fas fa-tractor"> Farm Fresh</i></a>
             <a href=''>Shop</a>
             <a href=''>Farms</a>
-            <a href=''>Account</a>
             <a href=''>Search</a>
             <a href=''><i class="fas fa-shopping-cart"></i></a>
-            <a href='/sign-in'>Sign In</a>
+            <a href='/login'>Sign In</a>
             <button className='sellBtn'><a href=''>Click Here to Sell</a></button>
         </nav>
     )
