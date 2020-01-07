9ef0bbba834850ef6096ea4509d2024f3f3b37fa
DeniseFafette <denisefafette@gmail.com>
2020-01-07 14:02:47 -0800

    commented out broken code and fixed broken imports

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React from 'react';
 import {Route, Switch} from 'react-router-dom';
 
 // component imports
-import RegisterForm from './components/RegisterForm';
-import SignInForm from './components/SignInForm';
+import RegisterForm from './components/CreateAcc';
+import Login from './components/Login';
 import NavBar from './components/NavBar';
+
 // style imports
 import './App.css';
 
@@ -12,15 +13,15 @@ function App() {
   return (
     <div className="App">
       <NavBar />
-      <Switch>
-        <Route exact path='/register-form'>
+      {/* <Switch>
+        <Route exact path='/createacc'>
           <RegisterForm />
         </Route>
-        <Route exact path='/sign-in'>
-          <SignInForm />
+        <Route exact path='/login'>
+          <Login />
         </Route>
         
-      </Switch>
+      </Switch> */}
     </div>
   );
 }
