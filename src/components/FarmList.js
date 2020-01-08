91b51b9a1a5b7f1e7dee865b11dd60b002e4f1b3
DeniseFafette <denisefafette@gmail.com>
2020-01-07 16:48:11 -0800

    commented out FarmList and refactored CreateAcc

---
+++ src/components/FarmList.js
@@ -0,0 +1,15 @@
+import React, { useEffect } from 'react';
+import axios from 'axios'
+
+const FarmList = props => {
+    
+    useEffect(() => {
+        axios.get
+    }, [])
+    return (
+        props.farm.data
+    
+    )
+}
+
+export default FarmList;
