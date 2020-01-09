cd4c8439c447969dad4b9b619991b5a5bfd49ab2
mlazzell17@gmail.com <MicheSi>
2020-01-09 14:24:23 -0700

    added alt to icons on nav and footer

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,11 +3,11 @@ import React from 'react';
 const NavBar = props => {
     return (
         <nav>
-            <a href='/'><i className="fas fa-tractor"> Farm Fresh</i></a>
+            <a href='/'><i className="fas fa-tractor" alt="tractor"> Farm Fresh</i></a>
             <a href=''>Shop</a>
             <a href='/farms'>Farms</a>
             <a href=''>About Us</a>
-            <a href=''><i className="fas fa-shopping-cart"></i></a>
+            <a href=''><i className="fas fa-shopping-cart" alt="shopping cart"></i></a>
             <a href='/login'>Sign In</a>
             <button className='sellBtn'><a href='/register'>Click Here to Register</a></button>
         </nav>
