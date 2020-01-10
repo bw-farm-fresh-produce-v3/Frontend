986d404aea0527b3ab54cf0abee1d28f08b27bb0
mlazzell17@gmail.com <MicheSi>
2020-01-10 09:01:18 -0700

    added ui links to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,10 +3,10 @@ import React from 'react';
 const NavBar = props => {
     return (
         <nav>
-            <a href='/'><i className="fas fa-tractor" alt="tractor"> Farm Fresh</i></a>
+            <a href='https://farm-fresh-produce3.netlify.com/'><i className="fas fa-tractor" alt="tractor"> Farm Fresh</i></a>
             <a href='/produce'>Shop</a>
             <a href='/farms'>Farms</a>
-            <a href=''>About Us</a>
+            <a href='https://farm-fresh-produce3.netlify.com/about.html'>About Us</a>
             <a href='/cart'><i className="fas fa-shopping-cart" alt="shopping cart"></i></a>
             <a href='/login'>Sign In</a>
             <button className='sellBtn'><a href='/register'>Click Here to Register</a></button>
